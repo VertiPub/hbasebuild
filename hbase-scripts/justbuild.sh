c63dc94b0eac435a51dac88830c162d1cd0b2715
Lee T. Tucker <ltucker@verticloud.com>
2013-02-12 16:26:13 -0800

    Remove -Dsnappy for now

--- hbase-scripts/justbuild.sh
+++ hbase-scripts/justbuild.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -ex
-MAVEN_OPTS="-Xmx2g" mvn clean site install assembly:assembly package -DskipTests -Dsnappy -Prelease -Drelease=${ARTIFACT_VERSION}
+MAVEN_OPTS="-Xmx2g" mvn clean site install assembly:assembly package -DskipTests -Prelease -Drelease=${ARTIFACT_VERSION}
